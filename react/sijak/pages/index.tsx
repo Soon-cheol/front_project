ed1badd30a3595c04f289b57b3735d5cc2f99e1a
Soon-cheol <sooncheolchoi7@gmail.com>
2022-11-11 13:21:19 +0900

    add scss

--- react/sijak/pages/index.tsx
+++ react/sijak/pages/index.tsx
@@ -1,11 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
     return (
         <div className={styles.container}>
-            ㅁㄴㅇㄹㄴㅇ132
+            ㅁㄴㅇㄹㄴㅇ132sfd
         </div>
     );
 }
