2ad80dea760541c1c00425a7515169808aa04a3c
Soon-cheol <sooncheolchoi7@gmail.com>
2022-11-11 14:09:50 +0900

    build path

--- react/sijak/next.config.js
+++ react/sijak/next.config.js
@@ -4,9 +4,14 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  distDir: "build",
+  exportTrailingSlash: true,
+  assetPrefix: "http://localhost:3000/",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
+
+// console.log('__dirname', __dirname);
 
 module.exports = nextConfig
