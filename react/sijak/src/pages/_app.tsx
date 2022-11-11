285fcb4eb0fd751d552829d36714a2d7364a331d
Soon-cheol <sooncheolchoi7@gmail.com>
2022-11-11 15:45:28 +0900

    css path

--- react/sijak/src/pages/_app.tsx
+++ react/sijak/src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import "/styles/globals.scss";
+import "/css/globals.scss";
+import "/css/index.scss";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- react/sijak/src/pages/index.tsx
+++ react/sijak/src/pages/index.tsx
@@ -1,8 +1,8 @@
-import styles from "/styles/Home.module.scss";
+// import "/css/index.scss";
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <div className='container'>
             ㅁㄴㅇㄹ234
         </div>
     );
